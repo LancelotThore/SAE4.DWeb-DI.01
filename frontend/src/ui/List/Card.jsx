ea92757165696277adc0e9176658c33e3681e2df
LancelotThore <144225580+LancelotThore@users.noreply.github.com>
2024-04-01 22:55:32 +0200

    it-1

--- frontend/src/ui/List/Card.jsx
+++ frontend/src/ui/List/Card.jsx
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 export default function Card({ data }) {
     return (
         <>
-            <Link to={`/${data.id}`} className="w-full h-full">
                 <img className="h-full w-full object-cover rounded" src={`/affiches/${data.Image}`} alt={data.name} />
                 <div className="absolute w-full h-full hover:border-4 hover:border-border top-0 rounded">
                     <p className="absolute w-6 h-6 bg-border invisible hover:visible z-30 text-center right-0 m-2 rounded-full text-openNavBackground">i</p>
                 </div>
-            </Link>
         </>
     );
 }
--- frontend/src/ui/List/index.jsx
+++ frontend/src/ui/List/index.jsx
@@ -3,25 +3,21 @@ import Card from "./Card";
 import Info from './Info';
 
 export default function List({movies=[]}) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [showInfo, setShowInfo] = useState(false);
 
-    function goToLeft() {
-        setCurrentIndex(oldIndex => oldIndex === 0 ? movies.length - 1 : oldIndex - 1);
-    }
-
-    function goToRight() {
-        setCurrentIndex(oldIndex => oldIndex === movies.length - 1 ? 0 : oldIndex + 1);
+    function handleCardClick() {
+        setShowInfo(true);
     }
 
     return (
         <section className="my-3 mx-7">
             <p className="pb-2">A ne pas manquer</p>
-            <Info />
+            {showInfo && <Info />}
             <div className="flex overflow-x-scroll hide-scrollbar">
                 <ul className="flex gap-2 flex-nowrap">
                     {movies.map((movie, index) => (
-                        <li className="w-32 h-44 md:w-40 md:h-52 relative" key={movie.id}>
-                            <Card data={movie} />
+                        <li className="w-32 h-44 md:w-40 md:h-52 relative" onClick={handleCardClick} key={movie.id}>
+                            <Card data={movie}/>
                         </li>
                     ))}
                 </ul>
